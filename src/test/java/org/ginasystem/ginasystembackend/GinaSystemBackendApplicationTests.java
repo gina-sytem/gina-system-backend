b8d2145845fdda369fa37cb9ee2647e9d8f76d1e
UnDer-7 <mateus7532@gmail.com>
2024-08-24 22:06:19 -0300

    project created

---
+++ src/test/java/org/ginasystem/ginasystembackend/GinaSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ginasystem.ginasystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GinaSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
