85ac148ee6c56a964d513eb3196e4d4bf0639b2e
Mateus Gomes <mateus7532@gmail.com>
2024-09-06 20:42:14 -0300

    config swagger (#8)

--- src/main/java/org/ginasystem/ginasystembackend/GinaSystemBackendApplication.java
+++ src/main/java/org/ginasystem/ginasystembackend/GinaSystemBackendApplication.java
@@ -1,8 +1,26 @@
 package org.ginasystem.ginasystembackend;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Gina System - Backend API",
+		description = "Backend do Gina System",
+		contact = @Contact(
+			url = "toDo Colocar site de suporte",
+			name = "Gina System support"
+		)
+	),
+	externalDocs = @ExternalDocumentation(
+		url = "https://github.com/gina-system/gina-system-backend/wiki",
+		description = "Wiki do projeto"
+	)
+)
 @SpringBootApplication
 public class GinaSystemBackendApplication {
 
